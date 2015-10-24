a47e2a91d95c6d6987bf62dfa4fafabd705b9134
Johannes Stein <johannesstein@freeze-dev.com>
2015-10-24 16:11:51 +0200

    Use BackgroundImage

--- src/components/scene/Introduction.js
+++ src/components/scene/Introduction.js
@@ -1,12 +1,13 @@
 import React, {Component} from 'react';
 
 import Scene from './Scene';
+import BackgroundImage from '../BackgroundImage';
 
 class Introduction extends Component {
   render() {
     return (
       <Scene name="introduction">
-
+        <BackgroundImage src="images/introduction.png" />
       </Scene>
     )
   }
